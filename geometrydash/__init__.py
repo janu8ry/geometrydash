412f9bdc68308f3036d5dcd324772a349d3f917a
janu8ry <janu8ry0108@gmail.com>
2021-02-12 16:19:12 +0900

    fixed typo

--- geometrydash/__init__.py
+++ geometrydash/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'geometrydash'
 __author__ = 'janu8ry'
 __license__ = 'GPL-3.0'
 __copyright__ = 'Copyright 2021 janu8ry'
-__version__ = '0.1a'
+__version__ = '1.0.0'
 
 from collections import namedtuple
 
@@ -21,9 +21,9 @@ from .errors import *
 from .geometrydash import *
 from .objects import *
 
-VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+VersionInfo = namedtuple('VersionInfo', 'major minor micro')
 
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='alpha', serial=0)
+version_info = VersionInfo(major=1, minor=0, micro=0)
 
 
 
